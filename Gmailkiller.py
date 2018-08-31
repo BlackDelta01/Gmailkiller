e979becacfe8f68421e71035dbb8c96bd5db8a7a
BlackDelta01 <42820109+BlackDelta01@users.noreply.github.com>
2018-08-31 11:37:00 -0500

    Update Gmailkiller.py

--- Gmailkiller.py
+++ Gmailkiller.py
@@ -4,6 +4,15 @@ smtpserv = smtplib.SMTP("smtp.gmail.com" , 587)
 smtpserv.ehlo()
 smtpserv.starttls()
 
+banner = '''
+ / ___|_ __ ___   __ _(_) | |/ (_) | | ___ _ __
+| |  _| '_ ` _ \ / _` | | | ' /| | | |/ _ \ '__|
+| |_| | | | | | | (_| | | | . \| | | |  __/ |
+ \____|_| |_| |_|\__,_|_|_|_|\_\_|_|_|\___|_|
+==================BlackDelta====================
+=================================================
+
+                                                                      '''
 
 user = raw_input("Enter the target's email address : ")
 passwordfile =raw_input("Enter the path of the password list : ")
